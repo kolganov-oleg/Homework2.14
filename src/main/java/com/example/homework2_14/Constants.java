eb89e7f8529e33c48c591d43e4a9f9bd7257e507
kolgan <6775366@gmail.com>
2022-11-14 01:44:19 +0300

    2.14 start

---
+++ src/main/java/com/example/homework2_14/Constants.java
@@ -0,0 +1,8 @@
+package com.example.homework2_14;
+
+public class Constants {
+    public static final String LEGITIMATE_STRING = "completely legal";
+    public static final int LEGITIMATE_INDEX = 0;
+    public static final int ILLEGAL_INDEX = -1;
+    public static final String ILLEGAL_STRING = null;
+}
